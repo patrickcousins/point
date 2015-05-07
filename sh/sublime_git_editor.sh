8486b4677a6aee6c97cdefee681a12f6befef432
patrickcousins <patrick@seeclickfix.com>
2015-05-07 12:13:37 -0400

    Put the subl symlink in the brew bin folder

--- sh/sublime_git_editor.sh
+++ sh/sublime_git_editor.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
-cd ~
-mkdir -p bin
-PATH=$PATH:~/bin
-export PATH
+#!/bin/sh
+#cd ~
+#mkdir -p bin
+#PATH=$PATH:~/bin
+#export PATH=$PATH:~/bin
+ln -svf "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 git config --global core.editor "subl -n -w"
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+echo "Adding subl symlink to your /usr/local/bin/subl"
+echo "Make sure this folder is in your PATH"
